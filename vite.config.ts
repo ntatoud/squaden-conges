26ea441cc90f33802d04d1b4018f766bfa3ed758
Noe TATOUD <noe.tatoud@gmail.com>
2025-12-22 19:59:18 +0100

    fix: remove nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { codeInspectorPlugin } from 'code-inspector-plugin';
-import cpy from 'cpy';
-import { Nitro } from 'nitro/types';
-import { nitro } from 'nitro/vite';
-import { resolve } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
-const { nitroRetrieveServerDirHook, prismaCopyBinariesPlugin } =
-  createPrismaCopyBinariesPlugin();
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -25,46 +19,15 @@ export default defineConfig(({ mode }) => {
       codeInspectorPlugin({
         bundler: 'vite',
       }),
+      netlify(),
       tsConfigPaths(),
       tanstackStart(),
-      netlify(),
-      nitro({
-        config: {
-          hooks: {
-            'build:before': nitroRetrieveServerDirHook,
-          },
-          routeRules: { '/storybook': { redirect: '/storybook/index.html' } },
-        },
-      }),
       // react's vite plugin must come after start's vite plugin
       viteReact({
         babel: {
           plugins: ['babel-plugin-react-compiler'],
         },
       }),
-      // Copy prisma binaries at the end
-      prismaCopyBinariesPlugin(),
     ],
   };
 });
-
-function createPrismaCopyBinariesPlugin() {
-  let serverDir = '';
-  return {
-    nitroRetrieveServerDirHook: (nitro: Nitro) => {
-      serverDir = nitro.options.output.serverDir.replace(resolve('.'), '.');
-    },
-    prismaCopyBinariesPlugin: () => ({
-      name: 'prisma-copy-binaries',
-      writeBundle: async (outputOptions: { dir?: string }) => {
-        const outputDir = outputOptions.dir?.replace(resolve('.'), '.');
-        if (outputDir === serverDir) {
-          await cpy(
-            './src/server/db/generated/**/*.node',
-            resolve(serverDir, 'chunks')
-          );
-        }
-      },
-    }),
-  };
-}
